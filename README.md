15dd34a9ca2d83c5bffa73ccec395cf9da25f30a
Mark6132014 <154909794+Mark6132014@users.noreply.github.com>
2024-06-07 16:15:28 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Welcome to Windows 8!
+### Everybody says that Windows 8 is bad, and not gooder than Windows ME. But it ain't that. It's Windows 8.1!
+# Enjoy Windows 8!
---
+++ index.js
@@ -0,0 +1,32 @@
+console.log("%cSTOP!\nIf something is wrong, you can debug it using the Inspect element.\nWebsite: %o", "color: red; background: white", "https://itzgametime-vip.w3spaces.com");
+setInterval(() => {
+    document.querySelector('.taskbar .time').innerHTML = new Date().getHours() + ':' + new Date().getMinutes() + ':' + new Date().getSeconds() + '<br>' + new Date().getMonth() + '/' + new Date().getDay() + '/' + new Date().getFullYear();
+}, 1000);
+var taskbar = {
+    startSearch: function() {
+        document.querySelector(".start").style.display = "block";
+    },
+    browser: function() {
+        document.querySelector(".ms-edge-application").style.display = "block";
+    }
+}
+var startbuttons = document.querySelector(".taskbar .startNow");
+startbuttons.addEventListener("click", () => {
+    document.querySelector(".start").style.display = "block";
+});
+var browser = document.querySelector(".ms-edge-application");
+setTimeout(() => {
+    document.querySelector(".startup #start").style.display = "block";
+}, 3000);
+browser.querySelector("#go").addEventListener("click", () => {
+    let address = browser.querySelector("#web-address");
+    let website = browser.querySelector("#website");
+    website.src = address.value;
+});
+var music = new Audio("Sounds/music.mp3");
+document.querySelector(".start .cardMusic #play").addEventListener("click", () => {
+    music.play();
+});
+document.querySelector(".start .cardMusic #pause").addEventListener("click", () => {
+    music.pause();
+});
